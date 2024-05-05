7f18c653ec9a6afdcd54b17f5609e891627a3ebb
Polina <prrshka@mail.ru>
2024-05-06 02:00:56 +0300

    добавлена попытка работы с GUI как из видео 3

--- main.cpp
+++ main.cpp
@@ -1,45 +1,20 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <ctime>
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
 
 using namespace std;
 
-//создаём массив в динамической памяти и заполняем его рандомными значениями от 0 до 100
-vector <int> mas(int n) {
-    vector <int> a(n);
-    srand(time(0));
-    for (int i = 0; i < n; i++)
-        a[i] = rand()%100;
-    return a;
+int exe(QApplication& app) {
+    return app.exec();
 }
 
-void gistogram(int k) {
-    vector <int> data = mas(k);//количество элементов в массиве
+int main(int arge, char **argv){
+    cerr << "create app \n ";
+    QApplication app(arge, argv);
 
-    int max_value = *max_element(data.begin(), data.end());//находит максимальный элемент в массиве
+    int result = exe(app);
+    cerr << " at qpp redult: " << result << endl;
+    return result;
 
-    for (int i = max_value; i >= 0; --i) {
-        cout.width(2);
-        cout << i << " | ";
-
-        for (int j = 0; j < data.size(); ++j) {
-            if (data[j] == i) {
-                cout << "*";
-            }
-            else {
-                cout << "";
-            }
-        }//алгоритм построения гистограммы
-        cout << endl;
-    }
-    cout << "---------------------------------------" << endl;
-    cout << "  ";
-
-    return;
-}
-
-int main()
-{
-    gistogram(1000);
 }
